d72294af9e0402dfd9dc6734098968a927116c38
chicago-joe <jloss94@gmail.com>
2020-12-01 07:25:39 -0600

    created IB Trader Client for IB trading / portfolio mgmt

--- _source/ibClient/main.py
+++ _source/ibClient/main.py
@@ -4,19 +4,56 @@
 # Original Templates, Classes, and Parameters located at:
 # https://github.com/jamesmawm/High-Frequency-Trading-Model-with-IB
 #
-# from tmp_hft_model import HFTModel
-from ry_model import HFTModel
+# --------------------------------------------------------------------------------------------------
+#
+
+from ibClient import HftModel1
+import os
+from ib_insync import Stock, ContFuture, Future, Forex
+from fnCommon import setPandas, fnOdbcConnect, setLogging, fnUploadSQL
+
+
+# --------------------------------------------------------------------------------------------------
+# create IB contract symbiology
+
+def fnCreateIBSymbol(ticker_tk=None, ticker_at=None):
+    if not ticker_tk:
+        ticker_tk='SPY'
+    if not ticker_at:
+        ticker_at = 'EQT'
+
+    symIB = None
+
+    if ticker_at == 'EQT':
+        symIB = [ticker_tk, Stock(ticker_tk, 'SMART', 'USD')]
+    elif ticker_at == 'FUT':
+        symIB = [ticker_tk, Future(ticker_tk, 'SMART', 'USD')]
+
+    return symIB
+
+
+# --------------------------------------------------------------------------------------------------
+# read positon / direction from ML model
+
+# def fn
+
+# --------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
+# run main
 
 if __name__ == "__main__":
-    model = HFTModel(host = 'localhost',
-                     # port = 4002,
-                     port = 7497,
-                     client_id = 1,
-                     is_use_gateway = False,
-                     evaluation_time_secs = 20,
-                     resample_interval_secs = '30s')
 
-    model.start(['RDS A', 'BP'], 100)
-    # model.start(["JPM", "BAC"], 100)
-    # model.start(["SPY", "QQQ"], 100)
+    TWS_HOST = os.environ.get('TWS_HOST', '127.0.0.1')
+    TWS_PORT = os.environ.get('TWS_PORT', 7497)
 
+    print('Connecting on host:', TWS_HOST, 'port:', TWS_PORT)
+
+    # init model
+    model = HftModel1(
+            host = TWS_HOST,
+            port = TWS_PORT,
+            client_id = 1,
+    )
+    tickerIB = fnCreateIBSymbol(ticker_tk = 'SPY', ticker_at = 'EQT')
+
+    model.run(to_trade = tickerIB, trade_qty = 100)
